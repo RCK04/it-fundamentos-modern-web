c1e57067778b8ad1cb4cbac673e8028e40e26c84
Erick <erickkan2004@gmail.com>
2024-10-24 11:38:28 -0300

    Finish att1

--- att-1/src/js/script.js
+++ att-1/src/js/script.js
@@ -1,11 +1,5 @@
-// const image = document.querySelector("img");
+function changeImg(src){
+    document.querySelector(".prevImg").src = src;
+}
 
-// image.onclick = () => {
-//     let src = image.getAttribute("src");
-//     if(src === "./src/images/js-image.png"){
-//         image.setAttribute("src", "./src/images/css-image.png");
-    
-//     } else{
-//         image.setAttribute("src", "./src/images/js-image.png");
-//     }
-// }
+
